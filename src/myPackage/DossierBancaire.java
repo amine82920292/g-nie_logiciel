cef393cfbea3dd1a1ce99caf0d5f436ac01109c7
usrlocal <usrlocal@GA319-FE24.ad.univ-angers.fr>
2023-01-09 18:04:00 +0100

    Ajout de commentaires dans DossierBancaire

--- src/myPackage/DossierBancaire.java
+++ src/myPackage/DossierBancaire.java
@@ -1,7 +1,7 @@
 package myPackage;
 
 public class DossierBancaire {
-
+//attributs
 	private CompteCourant _cc;
 	private CompteEpargne _ce;
 	private double _soldeDB;
@@ -13,28 +13,29 @@ public class DossierBancaire {
 		_ce = new CompteEpargne();
 		_soldeDB = 0;
 	}
-
+//methode déposer
 	public void deposer(double value) {
 
 		_cc.Deposer(value * 0.4);
 		_ce.Deposer(value * 0.6);
 		_soldeDB += value;
 	}
+	//methode getsolde
 
 	public double get_solde() {
 		return _soldeDB;
 	}
-
+	//methode remunurer 
 	public void remunerer() {
 		double gain = _ce.remunerer();
 		this.deposer(gain);
 	
 	}
-
+	//methode pour recuperer les informations du compte courant
 	public CompteCourant get_cc() {
 		return _cc;
 	}
-
+	//methode pour recuperer les informations du compte épargne
 	public CompteEpargne get_ce() {
 		return _ce;
 	}
