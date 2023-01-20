4cef23a6ba20c271f372c54f86607c2d3de7d7b7
usrlocal <usrlocal@ga219-FE17.ad.univ-angers.fr>
2023-01-20 18:18:12 +0100

    Ajout des getters et setters des attribus de la classe banquier

--- src/myPackage/Banquier.java
+++ src/myPackage/Banquier.java
@@ -1,5 +1,38 @@
 package myPackage;
 
+import java.util.ArrayList;
+
 public class Banquier {
 
+	private  String _nom;
+	private  String _prenom;
+	private int id;
+	private ArrayList<DossierBancaire> dossiersbancaire ;
+	
+	//getters
+	public String get_nom() {
+		return _nom;
+	}
+	public void set_nom(String _nom) {
+		this._nom = _nom;
+	}
+	public String get_prenom() {
+		return _prenom;
+	}
+	public void set_prenom(String _prenom) {
+		this._prenom = _prenom;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<DossierBancaire> getDossiersbancaire() {
+		return dossiersbancaire;
+	}
+	public void setDossiersbancaire(ArrayList<DossierBancaire> dossiersbancaire) {
+		this.dossiersbancaire = dossiersbancaire;
+	}
+	
 }
